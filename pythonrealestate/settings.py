f1ca6c5aa597140d277244d0a5b0f37e023d3a70
Jas802 <jas.spencer414@gmail.com>
2021-03-27 13:42:36 -0400

    postgresql for migrations

--- pythonrealestate/settings.py
+++ pythonrealestate/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'pythonrealestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pyreal',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost'
     }
 }
 
